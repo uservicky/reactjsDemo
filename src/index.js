7f5a398c1884c4197e6a914cd7efc665a8a4854c
vicky sharma <uservickysharma@gmail.com>
2019-04-12 14:03:36 +0530

    2commoit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-
+//vicky
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
